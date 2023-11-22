690ba6ef3a1d954c24d9a992ff8e339816866a05
kongds <j614023177@icloud.com>
2023-11-22 23:09:44 +0800

    support hf hub

--- eval_only.sh
+++ eval_only.sh
@@ -1,54 +1,26 @@
 #!/bin/bash
 EXP=$1
 
-function gdrive-get() {
-    fileid=$1
-    filename=$2
-    if [[ "${fileid}" == "" || "${filename}" == "" ]]; then
-        echo "gdrive-curl gdrive-url|gdrive-fileid filename"
-        return 1
-    else
-        if [[ ${fileid} = http* ]]; then
-            fileid=$(echo ${fileid} | sed "s/http.*drive.google.com.*id=\([^&]*\).*/\1/")
-        fi
-        echo "Download ${filename} from google drive with id ${fileid}..."
-        cookie="/tmp/cookies.txt"
-        curl -c ${cookie} -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-        confirmid=$(awk '/download/ {print $NF}' ${cookie})
-        curl -Lb ${cookie} "https://drive.google.com/uc?export=download&confirm=${confirmid}&id=${fileid}" -o ${filename}
-        rm -rf ${cookie}
-        return 0
-    fi
-}
-
 case "$EXP" in
 "unsup-bert")
-    GDRIVE_CODE=1n9FULUIRBhmhvaSQPaOnsudb_CVZyBli
+    MODEL=royokong/unsup-PromptBERT
     ;;
 "unsup-roberta")
-    GDRIVE_CODE=16qQst04wAr_i59ZL-79CVXoivec4lZOZ
+    MODEL=royokong/unsup-PromptRoBERTa
     ;;
 "sup-bert")
-    GDRIVE_CODE=1TtqYSNeMpzQI59tqu3BNWUbnrkWB4GVm
+    MODEL=royokong/sup-PromptBERT
     ;;
 "sup-roberta")
-    GDRIVE_CODE=123wpRkpQr3OrlRuM2ZzeId2Mc-uw3ozY
+    MODEL=royokong/sup-PromptRoBERTa
     ;;
 *)
 esac
 
-if [ ! -d ${EXP} ]; then
-    echo "downloading " $EXP
-    gdrive-get $GDRIVE_CODE ${EXP}.zip
-    unzip ${EXP}.zip
-    rm ${EXP}.zip
-fi
-
-
 case "$EXP" in
 "unsup-bert")
     CUDA_VISIBLE_DEVICES=0 python evaluation.py \
-        --model_name_or_path  $EXP\
+        --model_name_or_path  $MODEL\
         --pooler avg\
         --mode test\
         --mask_embedding_sentence \
@@ -56,7 +28,7 @@ case "$EXP" in
     ;;
 "unsup-roberta")
     CUDA_VISIBLE_DEVICES=0 python evaluation.py \
-        --model_name_or_path  $EXP\
+        --model_name_or_path  $MODEL\
         --pooler avg\
         --mode test\
         --mask_embedding_sentence \
@@ -64,7 +36,7 @@ case "$EXP" in
     ;;
 "sup-bert")
     CUDA_VISIBLE_DEVICES=0 python evaluation.py \
-        --model_name_or_path  $EXP\
+        --model_name_or_path  $MODEL\
         --pooler avg\
         --mode test\
         --mask_embedding_sentence \
@@ -74,7 +46,7 @@ case "$EXP" in
     ;;
 "sup-roberta")
     CUDA_VISIBLE_DEVICES=0 python evaluation.py \
-        --model_name_or_path  $EXP\
+        --model_name_or_path  $MODEL\
         --pooler avg\
         --mode test\
         --mask_embedding_sentence \
